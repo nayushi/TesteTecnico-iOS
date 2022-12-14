1c28bad93e4dd6fcb9f101aa443a566059b418f3
Ivan De Martino <ivan.martino@outlook.com>
2022-12-14 15:49:46 -0300

    add custom cell

--- TesteTecnicoiOS/Home/HomeViewController.swift
+++ TesteTecnicoiOS/Home/HomeViewController.swift
@@ -14,19 +14,42 @@ class HomeViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        tableView.register(HomeTableViewCell.self,
+                           forCellReuseIdentifier: "cell")
         viewModel.viewController = self
         viewModel.fetchData()
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
         return viewModel.model.count
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let item = viewModel.model[indexPath.row]
-        let cell = UITableViewCell()
-        cell.textLabel?.text = item.name
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as? HomeTableViewCell else {
+            fatalError()
+        }
+        cell.nameLabel.text = item.name
+        cell.descriptionLabel.text = "\(item.status) - \(item.species)"
+        switch item.status {
+        case "Alive":
+            cell.decorationView.backgroundColor = .systemGreen
+        case "Dead":
+            cell.decorationView.backgroundColor = .red
+        default:
+            cell.decorationView.backgroundColor = .gray
+        }
+        if let data = try? Data(contentsOf: URL(string: item.image)!) {
+            cell.profileImage.image = UIImage(data: data)
+        }
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 72
     }
 }
 
